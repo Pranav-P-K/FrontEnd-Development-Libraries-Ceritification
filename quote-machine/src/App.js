66e32fc6953c6176e9f50302a920e518246e3b70
Pranav P K <pranavpk1519@gmail.com>
2024-09-22 21:15:49 +0530

    Quote machine created but api not working

--- quote-machine/src/App.js
+++ quote-machine/src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchRandomQuote = async () => {
+    const response = await fetch('andruxnet-random-famous-quotes.p.rapidapi.com');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
   return (
-    <div className="App">
-      
+    <div className="quote-box">
+      <div className="quote-text">
+        <span id="text">{quote}</span>
+      </div>
+      <div className="quote-author">
+        - <span id="author">{author}</span>
+      </div>
+      <div className="buttons">
+        <a
+          className="button"
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text="${quote}" - ${author}`}
+          title="Tweet this quote!"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img width="25" height="25" src="https://img.icons8.com/ios/50/twitterx--v1.png" alt="twitterx--v1"/>
+          Tweet
+        </a>
+        <button className="button" id="new-quote" onClick={fetchRandomQuote}>
+          New Quote
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
