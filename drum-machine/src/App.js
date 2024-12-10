fec19a72f82ecca5ed335e9374cc3857ea5ee8ad
Pranav P K <pranavpk1519@gmail.com>
2024-12-10 19:16:43 +0530

    drum machine created successfully

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import drumSounds from "./drum-sounds";
 
-function App() {
+const DrumPad = ({ sound, handleDisplay }) => {
+  const playSound = () => {
+    const audio = document.getElementById(sound.key);
+    audio.currentTime = 0;
+    audio.play();
+    handleDisplay(sound.id);
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key.toUpperCase() === sound.key) playSound();
+    };
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, [sound]);
+
   return (
-    <div className="App">
-      
+    <div id={sound.id} className="drum-pad" onClick={playSound}>
+      {sound.key}
+      <audio id={sound.key} className="clip" src={sound.src}></audio>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [display, setDisplay] = useState("");
+
+  const handleDisplay = (text) => {
+    setDisplay(text);
+  };
+
+  return (
+    <div id="drum-machine" className="container">
+      <div id="display" className="display">
+        {display || "Play a sound!"}
+      </div>
+      <div className="pads">
+        {drumSounds.map((sound) => (
+          <DrumPad key={sound.id} sound={sound} handleDisplay={handleDisplay} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
